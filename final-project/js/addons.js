9d43af91dfffd57127809e676e08ee078545968b
victorwa21 <victorwa@andrew.cmu.edu>
2023-12-08 11:43:33 -0500

    added alt text
    d

--- final-project/js/addons.js
+++ final-project/js/addons.js
@@ -5,21 +5,21 @@ $(document).ready(function () {
     localStorage.setItem('addonPets', 0);
 
     $(".dropdown-item").on("click", function () {
-        // Get the value associated with the clicked item
+        
         var selectedValue = $(this).data("value");
 
-        // Update the text of the dropdown button
+        
         var dropdownButton = $(this).closest(".dropdown").find(".dropdown-toggle");
         dropdownButton.text(selectedValue);
 
-        // Update localStorage based on the dropdown
+        
         if (dropdownButton.attr("id") === 'bicyclesDropdown') {
             localStorage.setItem('addonBicycles', selectedValue * 40);
         } else if (dropdownButton.attr("id") === 'petsDropdown') {
             localStorage.setItem('addonPets', selectedValue * 78);
         }
 
-        // Calculate the sum of both addons and update localStorage
+        
        
         
     });
